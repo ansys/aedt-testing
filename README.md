cbedfe87f767f9bfd386c57293b46b4b616dbbda
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-10-25 13:31:46 +0200

    added readme, reqs and first draft of main script

---
+++ README.md
@@ -0,0 +1,3 @@
+## Description
+Current project aims to provide an Automated Framework to test Ansys Electronics Desktop. User can set up a sweet of 
+tests to validate stability/regression of results between different versions of Electronics Desktop 
---
+++ aedttest.py
@@ -0,0 +1,23 @@
+import argparse
+import re
+__authors__ = "Maksim Beliaev, Bo Yang"
+
+
+def run(version):
+    """
+    Main function to start test sweet
+    Returns:
+    """
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # Add long and short argument
+    parser.add_argument("--aedt-version", "-av", help="Electronics Desktop version to test, e.g. 2022.1", required=True)
+    args = parser.parse_args()
+
+    aedt_version_pattern = re.compile(r"\d\d\d\d\.\d$")
+    if not aedt_version_pattern.match(args.aedt_version):
+        raise ValueError("Electronics Desktop version value is invalid. Valid format example: 2022.1")
+
+    run(version=args.aedt_version)
