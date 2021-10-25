347912f0a9634aedcf3880fe58930c96e9a481f6
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-10-25 20:28:58 +0200

    limits on resources (#8)
    * limit resource per run
    * add proper lock
    * added example configuration

--- aedttest/aedttest.py
+++ aedttest/aedttest.py
@@ -1,40 +1,98 @@
 import argparse
+import json
 import os
 import platform
 import re
 import shutil
 import subprocess
 import threading
+from collections import namedtuple
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
 __authors__ = "Maksim Beliaev, Bo Yang"
 
+from time import sleep
+
 ROOT_DIR = Path(__file__).resolve().parent.parent
 MODULE_DIR = Path(__file__).resolve().parent
 
 
-def run(version: str) -> None:
+thread_tuple = namedtuple("thread", ["thread", "cores", "ram"])
+
+
+def run(version: str, max_cores: int, max_ram: int, max_tasks: int, config_file: str) -> None:
     """
     Main function to start test sweet
     Returns: None
     """
 
+    with open(config_file) as file:
+        tests_config = json.load(file)
+
     script = str(MODULE_DIR / "dummy.py")
     script_args = ""
-    project_paths = [ROOT_DIR / "input" / "just_winding.aedt", ROOT_DIR / "input" / "just_winding2.aedt"]
     with TemporaryDirectory() as temp_dir:
-        threads = []
-        for project_path in project_paths:
+        active_threads = []
+
+        for project_name, project_config in tests_config.items():
+            job_cores = int(project_config["cores"])
+            job_ram = int(project_config["RAM"])
+
+            lock_execution(project_name, active_threads, job_cores, job_ram, max_cores, max_ram, max_tasks)
+
+            print(f"Add project {project_name}")
+            project_path = resolve_project_path(project_name, project_config)
+
             shutil.copy2(project_path, temp_dir)
-
             tmp_proj = os.path.join(temp_dir, project_path.name)
-            thread_args = (version, script, script_args, tmp_proj)
-            threads.append(threading.Thread(target=execute_aedt, daemon=True, args=thread_args))
 
-        for thread in threads:
+            thread_args = (version, script, script_args, tmp_proj)
+            thread = threading.Thread(target=execute_aedt, daemon=True, args=thread_args)
             thread.start()
-            thread.join()
+            active_threads.append(thread_tuple(thread, job_cores, job_ram))
+
+        [th.thread.join() for th in active_threads]
+
+
+def lock_execution(project_name, active_threads, job_cores, job_ram, max_cores, max_ram, max_tasks):
+    active_cores = sum((th.cores for th in active_threads))
+    active_ram = sum((th.ram for th in active_threads))
+    next_active_cores = active_cores + job_cores
+    next_active_ram = active_ram + job_ram
+    while (
+        (max_cores and next_active_cores > max_cores)
+        or (max_ram and next_active_ram > max_ram)
+        or (max_tasks and len(active_threads) >= max_tasks)
+    ):
+
+        print(
+            f"{project_name} is waiting for resources.\n"
+            f"Active cores: {active_cores}, RAM: {active_ram} GB, tasks: {len(active_threads)}\n"
+            f"Limits are cores: {max_cores}, RAM: {max_ram} GB, tasks: {max_tasks}\n"
+            f"Next job requires cores: {job_cores}, RAM: {job_ram} GB, tasks: 1\n"
+        )
+        sleep(5)
+        for i, th in enumerate(active_threads):
+            if not th.thread.is_alive():
+                next_active_cores -= th.cores
+                next_active_ram -= th.ram
+                active_threads.pop(i)
+                break  # break since pop thread item
+
+
+def resolve_project_path(project_name, project_config):
+    if "path" in project_config:
+        project_path = Path(project_config["path"])
+        if not project_path.is_absolute():
+            project_path = ROOT_DIR / project_path
+    else:
+        project_path = ROOT_DIR / project_name + ".aedt"
+
+    if not project_path.exists():
+        raise FileExistsError(f"Project {project_path} doesn't exist")
+
+    return project_path.resolve()
 
 
 def execute_aedt(version: str, script: str, script_args: str, project_path: str) -> None:
@@ -98,10 +156,26 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     # Add long and short argument
     parser.add_argument("--aedt-version", "-av", help="Electronics Desktop version to test, e.g. 221", required=True)
+    parser.add_argument("--config-file", "-cf", help="config file path", required=True)
+    parser.add_argument("--max-ram", "-r", help="total RAM limit [GB]", type=int)
+    parser.add_argument("--max-cores", "-c", help="total number of cores limit", type=int)
+    parser.add_argument("--max-tasks", "-t", help="total number of parallel tasks limit", type=int)
     args = parser.parse_args()
+
+    if not (args.max_ram or args.max_cores or args.max_tasks):
+        print("No limits are specified for current job. This may lead to failure if you lack of license or resources")
 
     aedt_version_pattern = re.compile(r"\d\d\d$")
     if not aedt_version_pattern.match(args.aedt_version):
         raise ValueError("Electronics Desktop version value is invalid. Valid format example: 221")
 
-    run(version=args.aedt_version)
+    if not os.path.isfile(args.config_file):
+        raise ValueError(f"Configuration file does not exist: {args.config_file}")
+
+    run(
+        version=args.aedt_version,
+        max_cores=args.max_cores,
+        max_ram=args.max_ram,
+        max_tasks=args.max_tasks,
+        config_file=args.config_file,
+    )
