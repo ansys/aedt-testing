992f8c93848b67ee3ad95ba34b4076a8b7b8aeed
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-10-26 14:10:45 +0200

    create results with progress during run

--- aedttest/static/js/scripts.js
+++ aedttest/static/js/scripts.js
@@ -1,7 +1,5 @@
 (function($) {
     "use strict";
-
-
     $(function () {
         for (var nk = window.location, o = $(".nano-content li a").filter(function () {
             return this.href == nk;
@@ -16,58 +14,4 @@
                 .addClass("active");
         }
     });
-
-
-    /* 
-    ------------------------------------------------
-    Sidebar open close animated humberger icon
-    ------------------------------------------------*/
-
-    $(".hamburger").on('click', function() {
-        $(this).toggleClass("is-active");
-    });
-
-
-
-
-
-    /* TO DO LIST 
-    --------------------*/
-    $(".tdl-new").on('keypress', function(e) {
-        var code = (e.keyCode ? e.keyCode : e.which);
-        if (code == 13) {
-            var v = $(this).val();
-            var s = v.replace(/ +?/g, '');
-            if (s == "") {
-                return false;
-            } else {
-                $(".tdl-content ul").append("<li><label><input type='checkbox'><i></i><span>" + v + "</span><a href='#' class='ti-close'></a></label></li>");
-                $(this).val("");
-            }
-        }
-    });
-
-
-    $(".tdl-content a").on("click", function() {
-        var _li = $(this).parent().parent("li");
-        _li.addClass("remove").stop().delay(100).slideUp("fast", function() {
-            _li.remove();
-        });
-        return false;
-    });
-
-    // for dynamically created a tags
-    $(".tdl-content").on('click', "a", function() {
-        var _li = $(this).parent().parent("li");
-        _li.addClass("remove").stop().delay(100).slideUp("fast", function() {
-            _li.remove();
-        });
-        return false;
-    });
-
-
-    
-
-
-
 })(jQuery);
