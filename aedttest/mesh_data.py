0b97b97f21731812d066e14c313ca274d445ece7
Bo Yang <bo.yang@ansys.com>
2021-10-27 13:34:24 +0200

    desgin_list from pyAedt remove comments

--- aedttest/mesh_data.py
+++ aedttest/mesh_data.py
@@ -10,23 +10,6 @@ class AedtTestException(Exception):
     """Base class for exceptions in this module."""
 
 
-def get_designs(oProject):
-    """
-    get all the designs of the project. If no design, raise error
-    Args:
-        oProject:
-
-    Returns:
-        design_names list
-    """
-
-    design_names = oProject.GetTopDesignList()
-    if not design_names:
-        raise AedtTestException("Project has no design")
-
-    return design_names
-
-
 def get_single_setup_mesh_data(oDesign, var, setup, mesh_stats_file):
     oDesign.GenerateMesh(setup)
     oDesign.ExportMeshStats(setup, var, mesh_stats_file)
@@ -60,12 +43,9 @@ def get_all_setup_mesh_data(oDesign, design, design_dict, project_dir, project_n
         design_dict[design][setup]["mesh_data"] = mesh_data
 
 
-def extract_data(oProject, project_dir, project_name):
+def extract_data(oProject, project_dir, project_name, design_names):
     design_dict = {}
-    # get the designs, if no design raise error
-    design_names = get_designs(oProject)
 
-    # loop all design and  get all setups for each design
     for design in design_names:
         design_dict[design] = {}
         try:
@@ -77,19 +57,22 @@ def extract_data(oProject, project_dir, project_name):
 
 
 def main():
+    desktop = Desktop("2021.1", False, False)
+    project_name = desktop.project_list().pop()
+    design_names = desktop.design_list()
 
-    d = Desktop("2021.1", False, False)
-
-    oDesktop = d._main.oDesktop
+    oDesktop = desktop._main.oDesktop
     oProject = oDesktop.GetActiveProject()
-    project_name = oProject.GetName()
     project_dir = oProject.GetPath()
 
-    try:
-        design_dict = extract_data(oProject, project_dir, project_name)
-        project_dict.update(design_dict)
-    except AedtTestException as e:
-        project_dict["error_exception"].append(str(e))
+    if design_names:
+        try:
+            design_dict = extract_data(oProject, project_dir, project_name, design_names)
+            project_dict.update(design_dict)
+        except AedtTestException as e:
+            project_dict["error_exception"].append(str(e))
+    else:
+        project_dict["error_exception"].append("Project has no design")
 
     out_json = r"{}.json".format(project_name)
     with open(os.path.join(project_dir, out_json), "w") as outfile:
