614496b3e1b5a45e9a81f52792d5d4c30ccc1010
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-11-02 21:46:15 +0100

    update tests

--- unittests/test_aedttest.py
+++ unittests/test_aedttest.py
@@ -1,28 +1,25 @@
 from aedttest.aedttest import allocate_task
 from aedttest.aedttest import allocate_task_within_node
-from aedttest.aedttest import machines_dict
 from aedttest.clusters.job_hosts import get_job_machines
 
 
 def test_allocate_task_single():
     job_machines = get_job_machines("host1:15,host2:10")
-    machines_dict.clear()
-    for machine in job_machines:
-        machines_dict[machine.hostname] = machine.cores
+    machines_dict = {machine.hostname: machine.cores for machine in job_machines}
 
-    allocated_machines = allocate_task({"cores": 17})
+    allocated_machines = allocate_task({"cores": 17}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 15, "tasks": 1}, "host2": {"cores": 2, "tasks": 1}}
 
-    allocated_machines = allocate_task({"cores": 15})
+    allocated_machines = allocate_task({"cores": 15}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 15, "tasks": 1}}
 
-    allocated_machines = allocate_task({"cores": 2})
+    allocated_machines = allocate_task({"cores": 2}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 2, "tasks": 1}}
 
-    allocated_machines = allocate_task({"cores": 25})
+    allocated_machines = allocate_task({"cores": 25}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 15, "tasks": 1}, "host2": {"cores": 10, "tasks": 1}}
 
-    allocated_machines = allocate_task({"cores": 26})
+    allocated_machines = allocate_task({"cores": 26}, machines_dict)
     assert not allocated_machines
 
 
@@ -33,20 +30,18 @@ def test_allocate_task_multiple():
 
     """
     job_machines = get_job_machines("host1:20,host2:10")
-    machines_dict.clear()
-    for machine in job_machines:
-        machines_dict[machine.hostname] = machine.cores
+    machines_dict = {machine.hostname: machine.cores for machine in job_machines}
 
-    allocated_machines = allocate_task({"cores": 16, "parametric_tasks": 2})
+    allocated_machines = allocate_task({"cores": 16, "parametric_tasks": 2}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 16, "tasks": 2}}
 
-    allocated_machines = allocate_task({"cores": 24, "parametric_tasks": 2})
+    allocated_machines = allocate_task({"cores": 24, "parametric_tasks": 2}, machines_dict)
     assert not allocated_machines
 
-    allocated_machines = allocate_task({"cores": 10, "parametric_tasks": 2})
+    allocated_machines = allocate_task({"cores": 10, "parametric_tasks": 2}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 10, "tasks": 2}}
 
-    allocated_machines = allocate_task({"cores": 25, "parametric_tasks": 5})
+    allocated_machines = allocate_task({"cores": 25, "parametric_tasks": 5}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 20, "tasks": 4}, "host2": {"cores": 5, "tasks": 1}}
 
     job_machines = get_job_machines("host1:10,host2:15")
@@ -54,27 +49,25 @@ def test_allocate_task_multiple():
     for machine in job_machines:
         machines_dict[machine.hostname] = machine.cores
 
-    allocated_machines = allocate_task({"cores": 26, "parametric_tasks": 2})
+    allocated_machines = allocate_task({"cores": 26, "parametric_tasks": 2}, machines_dict)
     assert not allocated_machines
 
-    allocated_machines = allocate_task({"cores": 10, "parametric_tasks": 2})
+    allocated_machines = allocate_task({"cores": 10, "parametric_tasks": 2}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 10, "tasks": 2}}
 
-    allocated_machines = allocate_task({"cores": 25, "parametric_tasks": 5})
+    allocated_machines = allocate_task({"cores": 25, "parametric_tasks": 5}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 10, "tasks": 2}, "host2": {"cores": 15, "tasks": 3}}
 
 
 def test_allocate_task_within_node():
     job_machines = get_job_machines("host1:15,host2:10")
-    machines_dict.clear()
-    for machine in job_machines:
-        machines_dict[machine.hostname] = machine.cores
+    machines_dict = {machine.hostname: machine.cores for machine in job_machines}
 
-    allocated_machines = allocate_task_within_node({"cores": 17})
+    allocated_machines = allocate_task_within_node({"cores": 17}, machines_dict)
     assert not allocated_machines
 
-    allocated_machines = allocate_task_within_node({"cores": 15})
+    allocated_machines = allocate_task_within_node({"cores": 15}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 15, "tasks": 1}}
 
-    allocated_machines = allocate_task_within_node({"cores": 2})
+    allocated_machines = allocate_task_within_node({"cores": 2}, machines_dict)
     assert allocated_machines == {"host1": {"cores": 2, "tasks": 1}}
