afc925a1a74f1cadf7d0b431ab670f4dcaf56d44
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-01-02 14:55:08 +0100

    added test for task runner

--- tests/unittests/test_aedt_test_runner.py
+++ tests/unittests/test_aedt_test_runner.py
@@ -356,6 +356,30 @@ class TestElectronicsDesktopTester(BaseElectronicsDesktopTester):
                 },
             }
 
+    @mock.patch(
+        "aedttest.aedt_test_runner.ElectronicsDesktopTester.prepare_project_report",
+        wraps=lambda *a, **kw: {"error_exception": [], "slider_limit": 2},
+    )
+    @mock.patch("aedttest.aedt_test_runner.ElectronicsDesktopTester.render_project_html", wraps=lambda *a, **kw: None)
+    @mock.patch("aedttest.aedt_test_runner.ElectronicsDesktopTester.render_main_html", wraps=lambda *a, **kw: None)
+    @mock.patch("aedttest.aedt_test_runner.execute_aedt", wraps=lambda *a, **kw: None)
+    @mock.patch("aedttest.aedt_test_runner.time_now", wraps=lambda *a, **kw: "2021-12-31 20:16:04")
+    def test_task_runner(self, time_mock, aedt_execute_mock, render_main_mock, render_project_mock, prep_proj_mock):
+        self.aedt_tester.active_tasks = 5
+        self.aedt_tester.machines_dict = {"my_host": 10}
+        self.aedt_tester.report_data["projects"] = {"my_proj": {}}
+
+        self.aedt_tester.task_runner("my_proj", "my/path", {"distribution": None}, {"my_host": {"cores": 5}})
+
+        assert self.aedt_tester.report_data == {
+            "projects": {
+                "my_proj": {"time": "2021-12-31 20:16:04", "status": "success", "link": "my_proj.html", "delta": 2}
+            }
+        }
+        assert self.aedt_tester.active_tasks == 4
+        assert self.aedt_tester.machines_dict == {"my_host": 15}
+        assert render_main_mock.call_count == 2
+
 
 class TestCLIArgs:
     def setup(self):
