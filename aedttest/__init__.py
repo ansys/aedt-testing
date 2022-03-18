cbc5c706de60d1b6b9a33938c53524f5f5bcd291
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-03-18 20:08:47 +0100

    add deploy pipeline

--- aedttest/__init__.py
+++ aedttest/__init__.py
@@ -3,7 +3,14 @@ Ansys Electronics Desktop Testing Framework.
 Current module allows to setup and run automated tests and validate results across multiple versions of
 Ansys Electronics Desktop products, eg HFSS, Maxwell, Icepak, Q3D, etc
 """
+from pathlib import Path
 
-with open("version.txt") as file:
-    __version__ = file.readline().strip()
+version_file = Path(__file__).resolve().parent / "version.txt"
+
+if version_file.exists():
+    with open(version_file) as file:
+        __version__ = file.readline().strip()
+else:
+    __version__ = "0.0.1.alpha0"
+
 __authors__ = "Maksim Beliaev, Bo Yang"
