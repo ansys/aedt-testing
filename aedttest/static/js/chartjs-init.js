03f5d672de52bc730826d2aed008b46b6b2de3dc
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-06-24 00:40:51 +0200

    added autostyle

--- aedttest/static/js/chartjs-init.js
+++ aedttest/static/js/chartjs-init.js
@@ -1,106 +1,106 @@
 function create_line_chart(
-	ctx,
-	x_data,
-	x_label,
-	y_label,
-	version_ref,
-	y_data_ref,
-	version_now,
-	y_data_now,
-	diff
+  ctx,
+  x_data,
+  x_label,
+  y_label,
+  version_ref,
+  y_data_ref,
+  version_now,
+  y_data_now,
+  diff
 ) {
-	let datasets = [];
-	// push reference and difference only if the exist
-	if (y_data_ref && y_data_ref.length) {
-		datasets.push({
-				label: version_ref,
-				data: y_data_ref,
-				backgroundColor: 'transparent',
-				borderColor: 'rgba(220,53,69,0.75)',
-				borderWidth: 3,
-				pointStyle: 'circle',
-				pointRadius: 5,
-				pointBorderColor: 'transparent',
-				pointBackgroundColor: 'rgba(220,53,69,0.75)',
-			})
-	}
+  let datasets = [];
+  // push reference and difference only if the exist
+  if (y_data_ref && y_data_ref.length) {
+    datasets.push({
+      label: version_ref,
+      data: y_data_ref,
+      backgroundColor: "transparent",
+      borderColor: "rgba(220,53,69,0.75)",
+      borderWidth: 3,
+      pointStyle: "circle",
+      pointRadius: 5,
+      pointBorderColor: "transparent",
+      pointBackgroundColor: "rgba(220,53,69,0.75)",
+    });
+  }
 
-	datasets.push({
-				label: version_now,
-				data: y_data_now,
-				backgroundColor: 'transparent',
-				borderColor: 'rgba(40,167,69,0.75)',
-				borderWidth: 3,
-				pointStyle: 'circle',
-				pointRadius: 5,
-				pointBorderColor: 'transparent',
-				pointBackgroundColor: 'rgba(40,167,69,0.75)',
-			})
+  datasets.push({
+    label: version_now,
+    data: y_data_now,
+    backgroundColor: "transparent",
+    borderColor: "rgba(40,167,69,0.75)",
+    borderWidth: 3,
+    pointStyle: "circle",
+    pointRadius: 5,
+    pointBorderColor: "transparent",
+    pointBackgroundColor: "rgba(40,167,69,0.75)",
+  });
 
-	if (diff && diff.length) {
-		datasets.push({
-				label: "Difference",
-				data: diff,
-				backgroundColor: 'transparent',
-				borderColor: 'rgba(148,40,167,0.75)',
-				borderWidth: 3,
-				pointStyle: 'circle',
-				pointRadius: 5,
-				pointBorderColor: 'transparent',
-				pointBackgroundColor: 'rgba(148,40,167,0.75)',
-				hidden: true,
-			})
-	}
+  if (diff && diff.length) {
+    datasets.push({
+      label: "Difference",
+      data: diff,
+      backgroundColor: "transparent",
+      borderColor: "rgba(148,40,167,0.75)",
+      borderWidth: 3,
+      pointStyle: "circle",
+      pointRadius: 5,
+      pointBorderColor: "transparent",
+      pointBackgroundColor: "rgba(148,40,167,0.75)",
+      hidden: true,
+    });
+  }
 
-	return new Chart(ctx, {
-		type: 'line',
-		data: {
-			labels: x_data,
-			type: 'line',
-			defaultFontFamily: 'Montserrat',
-			datasets: datasets
-		},
-		options: {
-			responsive: true,
+  return new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: x_data,
+      type: "line",
+      defaultFontFamily: "Montserrat",
+      datasets: datasets,
+    },
+    options: {
+      responsive: true,
 
-			tooltips: {
-				mode: 'index',
-				titleFontSize: 12,
-				titleFontColor: '#000',
-				bodyFontColor: '#000',
-				backgroundColor: '#fff',
-				titleFontFamily: 'Montserrat',
-				bodyFontFamily: 'Montserrat',
-				cornerRadius: 3,
-				intersect: false,
-			},
-			legend: {
-				display: false,
-				labels: {
-					usePointStyle: true,
-					fontFamily: 'Montserrat',
-				},
-			},
-			scales: {
-				x: {
-					display: true,
-					title: {
-						display: true,
-						text: x_label,
-					},
-				},
-				y: {
-					display: true,
-					title: {
-						display: true,
-						text: y_label,
-					},
-				},
-			},
-			title: {
-				display: false,
-				text: 'Normal Legend'
-			}
-		}
-	});
+      tooltips: {
+        mode: "index",
+        titleFontSize: 12,
+        titleFontColor: "#000",
+        bodyFontColor: "#000",
+        backgroundColor: "#fff",
+        titleFontFamily: "Montserrat",
+        bodyFontFamily: "Montserrat",
+        cornerRadius: 3,
+        intersect: false,
+      },
+      legend: {
+        display: false,
+        labels: {
+          usePointStyle: true,
+          fontFamily: "Montserrat",
+        },
+      },
+      scales: {
+        x: {
+          display: true,
+          title: {
+            display: true,
+            text: x_label,
+          },
+        },
+        y: {
+          display: true,
+          title: {
+            display: true,
+            text: y_label,
+          },
+        },
+      },
+      title: {
+        display: false,
+        text: "Normal Legend",
+      },
+    },
+  });
 }
--- aedttest/static/js/common.js
+++ aedttest/static/js/common.js
@@ -1,37 +1,37 @@
-(function($) {
-    "use strict";
-    $(function () {
-        for (var nk = window.location, o = $(".nano-content li a").filter(function () {
+(function ($) {
+  "use strict";
+  $(function () {
+    for (
+      var nk = window.location,
+        o = $(".nano-content li a")
+          .filter(function () {
             return this.href == nk;
-        })
-            .addClass("active")
-            .parent()
-            .addClass("active"); ;) {
-            if (!o.is("li")) break;
-            o = o.parent()
-                .addClass("d-block")
-                .parent()
-                .addClass("active");
-        }
-    });
+          })
+          .addClass("active")
+          .parent()
+          .addClass("active");
+      ;
+
+    ) {
+      if (!o.is("li")) break;
+      o = o.parent().addClass("d-block").parent().addClass("active");
+    }
+  });
 })(jQuery);
 
-
-(function($) {
-    "use strict";
-    $(function () {
-        badge_change(5);
-    });
+(function ($) {
+  "use strict";
+  $(function () {
+    badge_change(5);
+  });
 })(jQuery);
 
-
-$('#threshold-slider').slider({
-	formatter: function(value) {
-		return 'Current value: ' + value;
-	}
+$("#threshold-slider").slider({
+  formatter: function (value) {
+    return "Current value: " + value;
+  },
 });
 
-
-$('#threshold-slider').on('slide', function(slideEvt) {
-	badge_change(slideEvt.value);
+$("#threshold-slider").on("slide", function (slideEvt) {
+  badge_change(slideEvt.value);
 });
