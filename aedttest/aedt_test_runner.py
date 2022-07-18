812367bb13818067cb4bf41b94a9abe9960fe2f3
Samuel Lopez <samuel.lopez@ansys.com>
2022-07-18 13:00:03 +0200

    - Add epsilon to avg_perc

--- aedttest/aedt_test_runner.py
+++ aedttest/aedt_test_runner.py
@@ -542,18 +542,16 @@ class ElectronicsDesktopTester:
                         difference = []
                         for ref, actual in zip(y_ref_data, curve_data["y_data"]):
                             difference.append(ref - actual)
-                            epsilon = 0
-                            if actual == 0:
+                            epsilon = 0.0
+                            if actual == 0.0:
                                 epsilon = 1e-10
-                                actual = float(actual)
                             max_delta = max(max_delta, abs(1 - ref / (actual + epsilon)))
 
                         max_delta_perc = round(max_delta * 100, 3)
-                        epsilon = 0
+                        epsilon = 0.0
                         mean_curve_data = mean(curve_data["y_data"])
-                        if mean(curve_data["y_data"]) == 0:
+                        if mean(curve_data["y_data"]) == 0.0:
                             epsilon = 1e-10
-                            mean_curve_data = float(mean_curve_data)
                         avg_perc = round(abs(1 - mean(y_ref_data) / (mean_curve_data + epsilon)), 3)
 
                         # take always integer since ticks are integers, and +1 to allow to slide
