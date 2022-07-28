8890c414d4b763f0884b206548bc0baaa5bf88fc
Maksim Beliaev <beliaev.m.s@gmail.com>
2022-07-28 18:06:01 +0300

    sort projects by name on main.html (#206)

--- tests/unittests/test_web_pages.py
+++ tests/unittests/test_web_pages.py
@@ -7,11 +7,15 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
 
+from aedttest import aedt_test_runner
+
 from .test_aedt_test_runner import TESTS_DIR
 from .test_aedt_test_runner import BaseElectronicsDesktopTester
 
 options = FirefoxOptions()
 options.headless = True
+
+html_base_path = "/html/body/div/div/div/section"
 
 
 class TestProjectWebPage(BaseElectronicsDesktopTester):
@@ -38,7 +42,7 @@ class TestProjectWebPage(BaseElectronicsDesktopTester):
 
     def test_simtime_link(self):
         simtime_link = self.driver.find_element(
-            by=By.XPATH, value="/html/body/div/div/div/section/div[2]/div/div/div[2]/div/table/tbody/tr[2]/td[2]/a"
+            by=By.XPATH, value=f"{html_base_path}/div[2]/div/div/div[2]/div/table/tbody/tr[2]/td[2]/a"
         )
         assert simtime_link.text == "00:00:07"
 
@@ -47,13 +51,13 @@ class TestProjectWebPage(BaseElectronicsDesktopTester):
 
     def test_sim_name(self):
         sim_name = self.driver.find_element(
-            by=By.XPATH, value="/html/body/div/div/div/section/div[2]/div/div/div[2]/div/table/tbody/tr[1]/td[1]"
+            by=By.XPATH, value=f"{html_base_path}/div[2]/div/div/div[2]/div/table/tbody/tr[1]/td[1]"
         )
         assert sim_name.text == "Maxwell2DDesign1:Setup1:xs=0.5mm"
 
     def test_mesh_link(self):
         mesh_link = self.driver.find_element(
-            by=By.XPATH, value="/html/body/div/div/div/section/div[3]/div/div/div[2]/div/table/tbody/tr[1]/td[2]/a"
+            by=By.XPATH, value=f"{html_base_path}/div[3]/div/div/div[2]/div/table/tbody/tr[1]/td[2]/a"
         )
         assert mesh_link.text == "133"
 
@@ -62,18 +66,53 @@ class TestProjectWebPage(BaseElectronicsDesktopTester):
 
     def test_mesh_name(self):
         mesh_name = self.driver.find_element(
-            by=By.XPATH, value="/html/body/div/div/div/section/div[3]/div/div/div[2]/div/table/tbody/tr[2]/td[1]"
+            by=By.XPATH, value=f"{html_base_path}/div[3]/div/div/div[2]/div/table/tbody/tr[2]/td[1]"
         )
         assert mesh_name.text == "Maxwell2DDesign1:Setup1:xs=6.000000000e-01mm"
 
     def test_plot_button(self):
-        button = self.driver.find_element(by=By.XPATH, value="/html/body/div/div/div/section/div[8]/div/div/button")
+        button = self.driver.find_element(by=By.XPATH, value=f"{html_base_path}/div[8]/div/div/button")
 
         assert button.text == "Maxwell2DDesign1:Loss Plot 1:SolidLoss:Pass=2 xs=0.6mm"
 
     def test_error_messages(self):
         msg = self.driver.find_element(
-            by=By.XPATH, value="/html/body/div/div/div/section/div[1]/div/div/div[2]/div/table/tbody/tr/td"
+            by=By.XPATH, value=f"{html_base_path}/div[1]/div/div/div[2]/div/table/tbody/tr/td"
         )
 
         assert msg.text == "Design:Maxwell2DDesign1 Variation: xs=6.000000000e-01mm Setup: Setup1 has no mesh stats"
+
+
+class TestMainWebPage(BaseElectronicsDesktopTester):
+    def setup_class(self):
+        self.aedt_tester = aedt_test_runner.ElectronicsDesktopTester(
+            version="212",
+            max_cores=9999,
+            max_parallel_projects=9999,
+            config_folder=TESTS_DIR / "input" / "configs",
+            out_dir=None,
+            save_projects=None,
+            only_reference=True,
+            reference_folder=None,
+        )
+        self.aedt_tester.only_reference = True
+
+        self.aedt_tester.initialize_results()
+        self.aedt_tester.render_main_html()
+
+        self.webpage = f"file:///{self.aedt_tester.results_path / 'main.html'}"
+        self.rel_path = str(self.aedt_tester.results_path).lower()
+        self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+        self.driver.get(str(self.webpage))
+
+    def teardown_class(self):
+        self.driver.close()
+
+    def test_projects_order(self):
+        project_names = ["01_voltage_control", "19", "2019R1", "expression_excitation", "just_winding"]
+
+        for i, name in enumerate(project_names, start=1):
+            elem = self.driver.find_element(
+                by=By.XPATH, value=f"{html_base_path}/div[1]/div/div/div[2]/div/table/tbody/tr[{i}]/td[1]"
+            )
+            assert elem.text == name
