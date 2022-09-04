f7ccb89575458ca5f8fa276411890df02164b332
Samuel Lopez <samuel.lopez@ansys.com>
2022-09-04 10:45:57 +0200

    - Fix Reference results file bug

--- aedttest/aedt_test_runner.py
+++ aedttest/aedt_test_runner.py
@@ -255,9 +255,10 @@ class ElectronicsDesktopTester:
                 # initialize integer for proper rendering
                 self.report_data["projects"][project_name]["delta"] = 0
                 self.report_data["projects"][project_name]["avg"] = 0
+                reference_path = Path(self.reference_data[project_name]["filepath"], project_name)
                 if project_name in self.reference_data:
                     copy_path_to(
-                        Path(self.reference_data[project_name]["filepath"], project_name),
+                        reference_path.resolve(),
                         self.reference_folder,
                     )
 
